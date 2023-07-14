315fa33546acc186ab4c833cecdb7cc410bb8c03
Paul Lesack <paul.lesack@ubc.ca>
2023-07-14 09:09:39 -0700

    Study subclasses dict

--- src/dataverse_utils/dvdata.py
+++ src/dataverse_utils/dvdata.py
@@ -7,13 +7,13 @@ import requests
 
 TIMEOUT = 100
 
-class Study:
+class Study(dict): #pylint: disable=too-few-public-methods
     '''
-    Dataverse record
+    Dataverse record. Dataverse study records are pure metadata so this
+    is represented with a dictionary.
     '''
     def __init__(self, pid: str,
-                 url:str, key:str,
-                 *args, **kwargs):
+                 url:str, key:str):
         '''
         pid : str
             Record persistent identifier: hdl or doi
@@ -22,12 +22,17 @@ class Study:
         key : str
             Dataverse API key with downloader privileges
         '''
-        self.pid = pid
-        self.url = url
-        self.key = key
+        self['pid'] = pid
+        self['url'] = url
+        self['key'] = key
+        self['orig_json'] = None
+        if not self['orig_json']:
+            self['orig_json'] = self._orig_json()
+        self['upload_json'] = self._upload_json
+        self['file_ids'] = [x['dataFile'].get('id') for x in self['orig_json']['files']]
+        self['file_persistentIds'] = self._get_file_pids()
 
-    @property
-    def orig_json(self) -> dict:
+    def _orig_json(self) -> dict:
         '''
         Latest study version record JSON. Retrieved from
         Dataverse installation so an internet connection
@@ -35,30 +40,34 @@ class Study:
         '''
         #curl -H "X-Dataverse-key:$API_TOKEN" /
         #$SERVER_URL/api/datasets/:persistentId/?persistentId=$PERSISTENT_IDENTIFIER
-        getjson = requests.get(self.url+'/api/datasets/:persistentId',
-                               headers={'X-Dataverse-key':self.key},
-                               params = {'persistentId': self.pid},
+        getjson = requests.get(self['url']+'/api/datasets/:persistentId',
+                               headers={'X-Dataverse-key':self['key']},
+                               params = {'persistentId': self['pid']},
                                timeout = TIMEOUT)
         getjson.raise_for_status()
         return getjson.json()['data']['latestVersion']
 
     @property
-    def upload_json(self)->dict:
+    def _upload_json(self)->dict:
         '''
         A Dataverse JSON record with with PIDs and other information stripped
         suitable for upload as a new Dataverse study record.
         '''
-        return {'datasetVersion': {'license': self.orig_json['license'],
-                                     'termsOfUse': self.orig_json['termsOfUse'],
-                                     'metadataBlocks': self.orig_json['metadataBlocks']
+        return {'datasetVersion': {'license': self['orig_json']['license'],
+                                     'termsOfUse': self['orig_json']['termsOfUse'],
+                                     'metadataBlocks': self['orig_json']['metadataBlocks']
                                      }
                   }
 
-    def get_file_ids(self, rec_json: dict)->list:
+    def _get_file_pids(self)->list:
         '''
         Returns a list of file ids representing the file
         objects in dataverse record
         '''
+        pids = [x['dataFile'].get('persistentId') for x in self['orig_json']['files']]
+        if not all(pids):
+            return None
+        return pids
 
 class File:
     '''
