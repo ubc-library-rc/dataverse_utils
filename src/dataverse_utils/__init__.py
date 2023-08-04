cf8d0d951024fff2bb0cb66000a96f94b5e6f3ac
Paul Lesack <paul.lesack@ubc.ca>
2023-08-04 14:39:33 -0700

    Increment version number

--- src/dataverse_utils/__init__.py
+++ src/dataverse_utils/__init__.py
@@ -5,5 +5,5 @@ Generalized dataverse utilities. Note that
 '''
 from dataverse_utils.dataverse_utils import *
 
-VERSION = (0,9,1)
+VERSION = (0,9,2)
 __version__ = '.'.join([str(x) for x in VERSION])
