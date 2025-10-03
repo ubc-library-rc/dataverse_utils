ee6827ef17a87279f882b3647e4fd8d3e7bd3dca
Paul Lesack <paul.lesack@ubc.ca>
2025-10-03 08:38:40 -0700

    Global user agent header in __init__

--- src/dataverse_utils/__init__.py
+++ src/dataverse_utils/__init__.py
@@ -4,6 +4,11 @@ Generalized dataverse utilities. Note that
 `import dataverse_utils.dataverse_utils`
 '''
 from dataverse_utils.dataverse_utils import *
+import sys
 
-VERSION = (0,19,0)
+VERSION = (0,19,1)
 __version__ = '.'.join([str(x) for x in VERSION])
+
+USERAGENT = (f'dataverse_utils/v{__version__} ({sys.platform.capitalize()}); '
+             f'Python {sys.version[:sys.version.find("(")-1]}')
+UAHEADER = {'User-agent' :USERAGENT}
