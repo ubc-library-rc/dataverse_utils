ebf520e8463a923c96e40b3a19a8dc999949d4c6
Paul Lesack <paul.lesack@ubc.ca>
2026-04-27 15:57:06 -0700

    Collections tests with magic mock

--- tests/tests_collections.py
+++ tests/tests_collections.py
@@ -9,6 +9,10 @@ APIFILE = pathlib.Path(pathlib.Path(__file__).parent,'dv_frankenstein_complete_a
 with open(APIFILE, encoding='utf-8') as w:
     APIDATA = json.load(w)
 
+VERSIONFILE = pathlib.Path(pathlib.Path(__file__).parent,'dv_frankenstein_complete_api_output_all_versions.json')
+with open(VERSIONFILE, encoding='utf-8') as w:
+    VERSIONDATA = json.load(w)
+
 API2 = pathlib.Path(pathlib.Path(__file__).parent,'get_collection_listing.json')
 with open(API2, encoding='utf-8') as w:
     COLL_LISTING = json.load(w)
@@ -16,13 +20,13 @@ with open(API2, encoding='utf-8') as w:
 
 class TestDvCollection(unittest.TestCase):
     def setUp(self):
-        self.dv_coll = c.DvCollection(url='borealisdata.ca', 
+        self.dv_coll = c.DvCollection(url='borealisdata.ca',
                                       coll='gallupcarleton')
         self.dv_coll.get_collections=MagicMock(return_value=None)
         self.dv_coll.__get_shortname=MagicMock(return_value=None)
         self.dv_coll.get_collection_listing = MagicMock(return_value=COLL_LISTING)
         self.dv_coll.get_studies=MagicMock(return_value={})
-        self.dv_coll.get_study_info = MagicMock(return_value=c.StudyMetadata(study_meta=APIDATA))
+        self.dv_coll.get_study_info = MagicMock(return_value=c.StudyMetadata(study_meta=APIDATA, all_versions=VERSIONDATA))
 
     def test_get_coll(self):
         self.assertEqual(self.dv_coll.get_collections(), None)
@@ -35,8 +39,7 @@ class TestDvCollection(unittest.TestCase):
 
 class TestStudyMetadata(unittest.TestCase):
     def setUp(self):
-        self.teststudy = c.StudyMetadata(study_meta=APIDATA)
-
+        self.teststudy = c.StudyMetadata(study_meta=APIDATA, all_versions=VERSIONDATA)
     def test_iscorr(self):
         self.assertIsInstance(self.teststudy, c.StudyMetadata)
     
@@ -46,10 +49,9 @@ class TestStudyMetadata(unittest.TestCase):
     def test_files(self):
         self.assertIsInstance(self.teststudy.files, list)
 
-
 class TestReadmeOutput(unittest.TestCase):
     def setUp(self):
-        self.teststudy = c.StudyMetadata(study_meta=APIDATA)
+        self.teststudy = c.StudyMetadata(study_meta=APIDATA, all_versions=VERSIONDATA)
         self.readme  = c.ReadmeCreator(self.teststudy)
 
     def test_rename_field(self):
